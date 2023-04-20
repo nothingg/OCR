dd8ab14c0c5a697fdf8e6a49683c5fc5718137f1
Bird-PC <bird_aries@hotmail.com>
2023-04-20 22:45:24 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pdf2image
+import pytesseract
+from pytesseract import Output, TesseractError
+
+pdf_path = "donate.pdf"
+
+images = pdf2image.convert_from_path(pdf_path)
+
+pil_im = images[0] # assuming that we're interested in the first page only
+
+ocr_dict = pytesseract.image_to_data(pil_im, lang='eng+tha', output_type=Output.DICT)
+# ocr_dict now holds all the OCR info including text and location on the image
+
+text = " ".join(ocr_dict['text'])
+
+print(text)
